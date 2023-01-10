44af0f884de3405958f09c04023b08a53b96e02b
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-10 16:55:34 +0900

    3.3 css 적용 방법

--- src/MinJeong/App.tsx
+++ src/MinJeong/App.tsx
@@ -6,15 +6,15 @@ const App = (props: Props) => {
   let msg = "World"
   const addResult = (x: string, y: string) => {
     return (
-      <div>
+      <div className="card card-body bg-light mb-3">
         {x} + {y} = {x + y}
       </div>
     )
   }
   return (
-    <div>
+    <div className="container">
       <h2>Hello {msg}</h2>
-      <hr />
+      <hr className="dash-style"/>
       {addResult("리액트를", "배워 봅시다")}
     </div>
   )
