2174643c6c273f44d3fe0b07452ff10b6c230ae8
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-12 21:58:04 +0900

    Study 4.4 이벤트 핸들러와 상태 변경

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 // import App from './App'
-import App from '../src/MinJeong/PropsTypes/App'
+import App from '../src/MinJeong/Counter/App'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
