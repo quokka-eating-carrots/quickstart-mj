db13437cc368e7edc5519a04bb8dc15c91573fdc
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-13 00:07:03 +0900

    Study 제어 컴포넌트와 비제어 컴포넌트

---
+++ src/MinJeong/Counter/App2.tsx
@@ -0,0 +1,27 @@
+import React, { useState, ChangeEvent } from 'react'
+
+type Props = {}
+
+const App2 = (props: Props) => {
+  const [x, setX] = useState<number>(0)
+  const [y, setY] = useState<number>(0)
+
+  const changeValue = (e: ChangeEvent<HTMLInputElement>) => {
+    let newValue: number = parseInt(e.target.value)
+    if (isNaN(newValue)) newValue = 0
+    if (e.target.id === "x") setX(newValue)
+    else setY(newValue)
+  }
+  return (
+    <div>
+      <h3>제어 컴포넌트</h3>
+      X : <input id="x" type="text" value={x} onChange={changeValue} />
+      <br />
+      Y : <input id="y" type="text" value={y} onChange={changeValue} />
+      <br />
+      결과 : <span>{x + y}</span>
+    </div>
+  )
+}
+
+export default App2
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 // import App from './App'
 import App from '../src/MinJeong/Counter/App'
+import App2 from '../src/MinJeong/Counter/App2'
+import App3 from './MinJeong/Counter/App3'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
+    <App2 />
+    <App3 />
   </React.StrictMode>,
 )
