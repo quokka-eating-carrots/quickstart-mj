4b45076cbaf0c641e6aff5612adea48c6e1c0d91
quokka-eating-carrots <ovzip7@gmail.com>
2023-01-26 19:34:45 +0900

    5.4.6 생명주기 메서드 예제 3

--- src/5 react-class-component/App.tsx
+++ src/5 react-class-component/App.tsx
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import Chatting from "./Chatting";
+import ErrorBoundary from "./ErrorBoundary";
+import UserList from "./UserList";
 
 class App extends Component<{}, {}> {
   state = {};
 
   render() {
     return (
-      <div>
-        <Chatting />
-      </div>
+      <ErrorBoundary>
+        <div>
+          참여 사용자: <UserList users={"mj"} />
+          <Chatting />
+        </div>
+      </ErrorBoundary>
     );
   }
 }
