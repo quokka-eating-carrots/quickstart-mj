af7677cc9b33cf2192225376a8cdae4b0627dc31
quokka-eating-carrots <ovzip7@gmail.com>
2023-02-07 14:56:04 +0900

    부석순 많관부... ❤

---
+++ src/9 react-router/components/Bss.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Bss = (props: Props) => {
+  return <div>{props.member}</div>;
+};
+
+export default Bss;
--- src/9 react-router/pages/Home.tsx
+++ src/9 react-router/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router";
+import Youtube from "react-youtube";
 
 type Props = {};
 type LocationStateType = {
@@ -12,7 +13,11 @@ const Home = (props: Props) => {
   const from = state ? state.from : "";
   return (
     <div className="card card-body">
-      <h2>Home</h2>
+      <h2>부석순 - 파이팅 해야지 많관부 ૮ .◜◡◝ ა</h2>
+      <Youtube
+        videoId={"mBXBOLG06Wc"}
+        opts={{ width: "1080", height: "720", playerVars: { autoplay: 1 } }}
+      />
       {from !== "" ? <h4>state.from: {from}</h4> : ""}
     </div>
   );
